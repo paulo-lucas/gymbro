a89e520dc98b633309720c5a9750dc24506a01ad
Paulo Cerqueira <paulolucas.2010@hotmail.com>
2022-08-15 18:01:04 -0300

    Adding Firestore

--- app/screens/Home/Home.tsx
+++ app/screens/Home/Home.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Alert } from 'react-native';
+import { Button } from 'react-native-paper';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 
 const HomeScreen: React.FC = () => {
+  const signOut = () => {
+    auth()
+      .signOut()
+      .then(() => console.log('User signed out!'));
+  };
+
+  const test = async () => {
+    await firestore()
+      .collection('workout')
+      .add({
+        title: 'test',
+        property: true,
+      })
+      .then(() => Alert.alert('Test', 'Success on send data'))
+      .catch(console.error);
+  };
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>HomeScreen</Text>
+      <Button onPress={signOut}>
+        <Text>Sign Out</Text>
+      </Button>
+      <Button onPress={test}>
+        <Text>Test firestore</Text>
+      </Button>
     </View>
   );
 };
